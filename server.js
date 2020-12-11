fb1478cdd2a1d06e8e8c2ddf6cc1de352844cb3d
saambiswal <saambiswal@gmail.com>
2020-12-11 21:58:39 +0000

    added docker file

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ const mockEvents = {
     ]
 };
 
+const Firestore = require("@google-cloud/firestore");
+
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
 
 
 
@@ -39,25 +47,44 @@ app.get('/version', (req, res) => {
 
 // mock events endpoint. this would be replaced by a call to a datastore
 // if you went on to develop this as a real application.
+//app.get('/events', (req, res) => {
+   // res.json(mockEvents);
+//});
 app.get('/events', (req, res) => {
-    res.json(mockEvents);
+    getEvents(req, res);
 });
+
 
 // Adds an event - in a real solution, this would insert into a cloud datastore.
 // Currently this simply adds an event to the mock array in memory
 // this will produce unexpected behavior in a stateless kubernetes cluster. 
+//app.post('/event', (req, res) => {
+    // create a new object from the json data and add an id
+    //const ev = { 
+      //  title: req.body.title, 
+       // description: req.body.description,
+       // id : mockEvents.events.length + 1
+     //}
+    // add to the mock array
+   // mockEvents.events.push(ev);
+    // return the complete array
+   // res.json(mockEvents);
+//});
+
 app.post('/event', (req, res) => {
     // create a new object from the json data and add an id
     const ev = { 
         title: req.body.title, 
         description: req.body.description,
-        id : mockEvents.events.length + 1
      }
-    // add to the mock array
-    mockEvents.events.push(ev);
-    // return the complete array
-    res.json(mockEvents);
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
 });
+
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
